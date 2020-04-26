b174e272a2a4b242e1e9a39de141b81fef9113e9
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-04-26 17:02:27 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, Response, jsonify
-from videoCamera import VideoCamera
 
 app = Flask(__name__)
-
-video_stream = VideoCamera()
 
 @app.route('/')
 def index():
@@ -13,17 +10,6 @@ def index():
 def detect():
     return render_template('detect.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(video_stream),
-        mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @app.route('/heatmap')
 def heatmap():
     return render_template('heatmap.html')
@@ -31,6 +17,6 @@ def heatmap():
 @app.route('/mask')
 def mask():
     return render_template('mask.html')
-    
+
 if __name__=='__main__':
     app.run(debug=True)
